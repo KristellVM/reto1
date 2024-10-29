e7d22e4ae7f44f657d1c0656a0bf55a9122b6d18
KristellVM <krisvm31@gmail.com>
2024-10-29 10:00:00 +0100

    crear division

--- Reto1/src/retos/Funciones2.java
+++ Reto1/src/retos/Funciones2.java
@@ -6,4 +6,11 @@ public class Funciones2 {
 		return num1*num2;
 	}
 	
+	public static double dividir(int num1, int num2) {
+		if(num1==0||num2==0) {
+			return 0;
+		}
+		else return num1/num2;
+	}
+	
 }
